b8920e50b5c4ec4fafaa9fd977416c69cb72e1b7
Dharani <dharani19.km@gmail.com>
2025-11-07 03:50:03 +0000

    Intial Commit register

---
+++ register.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	char username[20],char email[20];
+	printf("Enter username: ");
+	scanf("%s",&username);
+	printf("Enter email: ");
+	scanf("%s",&email);
+	printf("Username: %s\n Email: %s",username,email);
+}
